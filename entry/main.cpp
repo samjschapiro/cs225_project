c749ac90181cc29c62a03b5ac7af9023119a47a4
SumayT9 <sthak99@gmail.com>
2022-12-02 21:25:13 +0000

    temp

--- entry/main.cpp
+++ entry/main.cpp
@@ -8,7 +8,7 @@ int main() {
     // run everything
     unordered_map<string, vector<double>> LatLong = getLatLongMap("../data/airports.dat", 4);
     unordered_map< string, unordered_map<string, double>> airportGraph = buildAirportGraph("../data/routes.dat", LatLong, 2, 4);
-    std::cout<<a_star_search("SFO", "JFL", LatLong, airportGraph) << std::endl;
+    std::cout<<a_star_search("JFK", "JFL", LatLong, airportGraph) << std::endl;
 
 
 }
