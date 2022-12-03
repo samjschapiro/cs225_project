7c1e0ad3398c5758d76e3f06abfcc3554a5c94a1
SumayT9 <sthak99@gmail.com>
2022-12-03 04:23:47 +0000

    A* search bugs fixed

--- src/a*_search.cpp
+++ src/a*_search.cpp
@@ -6,12 +6,16 @@ using namespace std;
 double a_star_search(string source, string dest, unordered_map<string, vector<double>> LatLong, unordered_map<string, unordered_map<string, double>> connections){
     // min heap of <curr path distance + heuristic, curr path distance, curr airport>
     priority_queue<tuple<double, double, string>, vector<tuple<double, double, string>>, greater<tuple<double, double, string>>> heap;
+    std::cout << LatLong[dest][0] << std::endl;
     heap.push(make_tuple(heuristic(source, dest, LatLong), 0, source));
     unordered_map<string, double> airport_dist; // visited set with distances
     airport_dist[source] = 0; // distance to source airport is 0
     while (!heap.empty()) {
+        std::cout<<heap.size()<<std::endl;
         tuple<double, double, string> curr = heap.top();
         heap.pop();
+        std::cout<<heap.size()<<std::endl;
+        cout << "_______________" << endl;
         string curr_airport = get<2>(curr);
         double dist_to = get<1>(curr);
         if (curr_airport == dest){
@@ -20,6 +24,7 @@ double a_star_search(string source, string dest, unordered_map<string, vector<do
         for (pair<string, double> neighbors : connections[get<2>(curr)]){
             double new_dist_to = dist_to + neighbors.second;
             if (airport_dist.count(neighbors.first) == 0 || airport_dist[neighbors.first] > new_dist_to){
+                airport_dist[neighbors.first] = new_dist_to;
                 heap.push(make_tuple(new_dist_to + heuristic(neighbors.first, dest, LatLong), new_dist_to, neighbors.first));
             }
         }
@@ -28,11 +33,8 @@ double a_star_search(string source, string dest, unordered_map<string, vector<do
 }
 
 double heuristic(string source, string dest, unordered_map<string, vector<double>> LatLong){
+    
     vector<double> lat_long_src = LatLong[source];
     vector<double> lat_long_dest = LatLong[dest];
-    try{
-        return haversine(lat_long_src[0], lat_long_src[1], lat_long_dest[0], lat_long_dest[1]);
-    } catch (...){
-        return 1000;
-    }
+    return haversine(lat_long_src[0], lat_long_src[1], lat_long_dest[0], lat_long_dest[1]);
 }
